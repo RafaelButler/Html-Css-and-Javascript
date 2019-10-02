ea963ffd2989d585382ebdad4d27545170933f1d
RafaelButler <49237824+RafaelButler@users.noreply.github.com>
2019-10-02 17:01:13 -0300

    Add files via upload

---
+++ Responsive Navbar/main.js
@@ -0,0 +1,5 @@
+const barra = document.querySelector('.itens');
+const clicar = document.querySelector('.menu0lis');
+clicar.addEventListener('click', function(){
+    barra.classList.toggle('appear');
+});
